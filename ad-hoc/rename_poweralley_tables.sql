3798d45ed8fd1214329f364ba9e4e7362c0c534a
Dmitry Klionsky <dm.klionsky@gmail.com>
2015-09-14 16:37:50 +0300

    Implement tables renaming playbook

---
+++ ad-hoc/rename_poweralley_tables.sql
@@ -0,0 +1,7 @@
+rename table Admins to admins;
+rename table AuditEvents to auditevents;
+rename table BackendItems to backenditems;
+rename table LogEvents to logevents;
+rename table Notifications to notifications;
+rename table PasswordResetRequests to passwordresetrequests;
+rename table __MigrationHistory to __migrationhistory;
